550d6066b39d75d6f3853ba2674ad7e5b98587eb
jherbin <jacek.herbin@gmail.com>
2022-03-16 23:00:45 +0100

    3

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function figureSelection() {
-  if (document.getElementById('figureSelector2').checked) {
-    document.getElementById('size2').style.display = 'inline-block';
-    document.getElementById('size1').style.display = 'none';
-  } else {
-    document.getElementById('size2').style.display = 'none';
-    document.getElementById('size1').style.display = 'inline-block';
+function squareSelection(square) {
+  if (square.target.id == 'whiteIcon') {
+    document.getElementById('whiteSize').style.display = 'inline-block';
+    document.getElementById('redSize').style.display = 'none';
+  } else if (square.target.id == 'redIcon') {
+    document.getElementById('whiteSize').style.display = 'none';
+    document.getElementById('redSize').style.display = 'inline-block';
   }
 }
